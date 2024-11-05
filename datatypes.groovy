825319e4200fddb4226c478c30f2a9d668e010ee
Ezequiel Sagardia <ezequielsagardia@gmail.com>
2024-11-05 16:27:52 -0300

    datatypes

---
+++ datatypes.groovy
@@ -0,0 +1,47 @@
+//byte
+byte b = 10
+println b
+println Byte.MIN_VALUE
+println Byte.MAX_VALUE
+println "=============="
+//short
+short s = 100
+println s 
+println Short.MIN_VALUE
+println Short.MAX_VALUE
+println "================"
+//int
+int i = 1000
+println i
+println Integer.MIN_VALUE
+println Integer.MAX_VALUE
+println "================="
+//long
+long l = 100000L
+println l
+println Long.MIN_VALUE
+println Long.MAX_VALUE
+println "=================="
+//float
+float f = 100.002F
+println f
+println Float.MIN_VALUE
+println Float.MAX_VALUE
+println "===================="
+// double
+double d = 1000.0002
+println d
+println Double.MIN_VALUE
+println Double.MAX_VALUE
+println "===================="
+// char
+char c = '*'
+println c
+// Boolean
+boolean flag = true
+println flag
+// String
+String str = "Groovy"
+println str
+
+
